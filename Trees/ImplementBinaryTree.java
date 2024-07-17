63f956985ef30ddc77728524f533613a50f33e3e
Vinamra Vaswani <vinamravaswani@gmail.com>
2024-07-17 09:40:33 +0530

    Tree implementation

---
+++ Trees/ImplementBinaryTree.java
@@ -0,0 +1,83 @@
+package Trees;
+
+import java.util.*;
+
+class ImplementBinaryTree{
+
+    static Queue<Node> que = new LinkedList<>();
+
+    static class Node{
+        int data;
+        Node left, right;
+
+        Node(int val){
+            this.data = val;
+            this.left = this.right = null;
+        }
+    }
+
+    static class BinaryTree{
+        Node root = null;
+        Scanner val_scan = new Scanner(System.in);
+
+        void insert_values(){
+
+            int val = val_scan.nextInt();
+            this.root = new Node(val);
+
+            que.add(this.root);
+
+            while(que.isEmpty() == false){
+                Node cur = que.poll();
+
+                int left = val_scan.nextInt();
+                
+                if(left != -1){
+                    Node left_node = new Node(left);
+
+                    cur.left = left_node;
+
+                    que.add(left_node);
+                }
+
+                int right = val_scan.nextInt();
+
+                if(right != -1){
+                    Node right_node = new Node(right);
+
+                    cur.right = right_node;
+
+                    que.add(right_node);
+                }
+            }
+        }
+
+        void inorder_traversal(Node root){
+            if (root == null){
+                return;
+            }
+
+            if(root.left != null){
+                inorder_traversal(root.left);
+            }
+
+            System.out.println(root.data);
+
+            if (root.right != null){
+                inorder_traversal(root.right);
+            }
+
+        }
+
+    }
+
+    public static void main(String[] args){
+    
+        BinaryTree tree = new BinaryTree();
+        
+        tree.insert_values();
+
+        tree.inorder_traversal(tree.root);
+
+    }
+}
